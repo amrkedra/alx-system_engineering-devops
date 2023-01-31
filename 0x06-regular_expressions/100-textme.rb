cad8ac02500a5cac8ac40439fa5ef58964111fa3
amrkedra <amrkedra.1993@gmail.com>
2023-01-31 13:05:12 +0200

    0x06-regular_expressions

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,2 +1,2 @@
 #!/usr/bin/env ruby
-puts ARGV[0].scan(/(?<=from:|to:|flags:).+?(?=\])/).join(',')
+puts ARGV[0].scan(/from:([+]?[[:alnum:]]*).*to:([+]?[[:alnum:]]*).*flags:((?:-?[0-9]:?){5})/).join(',')
